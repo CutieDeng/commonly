94b51440180e14b526865440bd09e58c6a912478
Cutie Deng <CutieDeng@iCloud.com>
2022-09-30 02:48:21 +0800

    新增了 cutie-nc 工具.
    todo:
    
    - 增加 cutie-nc 使用的文档

---
+++ src/bin/cutie-nc.rs
@@ -0,0 +1,129 @@
+use std::{net::{SocketAddrV4, Ipv4Addr, TcpStream}, io::{stdin, Write, Read}, fs::File};
+
+fn main() {
+    // SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(a, b, c, d), port))
+    let mut connect_config = Box::new(ConnectConfig { target: None, }); 
+    let mut input = String::new(); 
+    let mut cache = Box::new([0u8; 1024]); 
+    loop {
+        stdin().read_line(&mut input).unwrap(); 
+        if input.starts_with("connect ") {
+            let mut values = [0u16; 5]; 
+            let mut maximum = 0; 
+            let mut tag = false; 
+            for (i, r) in input.split_ascii_whitespace().skip(1).map(|s| s.parse::<u16>()).enumerate() {
+                maximum = i; 
+                if i >= 5 {
+                    tag = true; 
+                    println!("\x1b[33;1m[!] warning: too many arguments for connect operator! \x1b[0m"); 
+                    break 
+                }
+                values[i] = match r {
+                    Ok(v) => {v},
+                    Err(e) => {
+                        tag = true;
+                        println!("\x1b[33;1m[!] warning: failing parse at the index {i} - {:?}\x1b[0m", e); 
+                        break 
+                    },
+                }
+            }
+            if !tag {
+                if maximum != 4 {
+                    println!("\x1b[33;1m[!] warning: too less arguments for connect operator! \x1b[0m");    
+                } else {
+                    let mut ipv4 = [0u8; 4]; 
+                    let mut tag = false; 
+                    for i in 0..ipv4.len() {
+                        ipv4[i] = match values[i].try_into() {
+                            Ok(i) => {i},
+                            Err(_e) => {
+                                println!("\x1b[33;1m[!] warning: failing parse at the index {i} - Int Overflow Error \x1b[0m"); 
+                                tag = false; 
+                                break; 
+                            },
+                        }
+                    }
+                    if !tag {
+                        let new_stream = TcpStream::connect(SocketAddrV4::new(
+                            Ipv4Addr::new(ipv4[0], ipv4[1], ipv4[2], ipv4[3]),
+                            values[4])
+                        ); 
+                        match new_stream {
+                            Ok(n) => {
+                                connect_config.target = Some(n); 
+                                println!("[-] connect build. "); 
+                            },
+                            Err(e) => {
+                                println!("\x1b[31;1m[e] error: {:?}\x1b[0m", e)
+                            },
+                        }
+                    }
+                }
+            }
+        } else if input.starts_with("file ") {
+            match connect_config.as_mut().target {
+                Some(ref mut tcp_stream) => {
+                    let input = (&input[5..]).trim_end(); 
+                    let f = File::open(input);
+                    match f {
+                        Ok(mut actual_file) => {
+                            loop {
+                                let r = actual_file.read(cache.as_mut_slice()).unwrap(); 
+                                if r == 0 { 
+                                    println!("[-] finish the file block write. "); 
+                                    break 
+                                }
+                                match tcp_stream.write(&cache[0..r]) {
+                                    Ok(_) => {},
+                                    Err(e) => {
+                                        println!("\x1b[31;1m[e] error: write {:?}\x1b[0m", e); 
+                                        break 
+                                    },
+                                }
+                            }
+                            tcp_read_simple(tcp_stream, cache.as_mut_slice()); 
+                        },
+                        Err(e) => {
+                            println!("\x1b[31;1m[e] error: file open failure - {:?}", e); 
+                        },
+                    }
+                }
+                None => println!("\x1b[31;1m[e] error: invalid status for sending file, because connection doesn't exist. \x1b[0m"), 
+            }
+        } else if input.starts_with(" ") {
+            match connect_config.as_mut().target {
+                Some(ref mut tcp_stream) => {
+                    match tcp_stream.write((&input[1..]).as_bytes()) {
+                        Ok(_) => {
+                        },
+                        Err(e) => {
+                            println!("\x1b[31;1m[e] error: {:?}\x1b[0m", e) 
+                        }
+                    }
+                    tcp_read_simple(tcp_stream, cache.as_mut_slice()); 
+                }
+                None => println!("\x1b[31;1m[e] error: invalid status for sending info, because connection doesn't exist. \x1b[0m"), 
+            }
+        } else {
+            println!("\x1b[33;1m{}{}\x1b[0m", "[!] warning: unexpected format for the raw str input: ", input.trim_end())
+        }
+        input.clear(); 
+    }
+}
+
+struct ConnectConfig {
+    target: Option<TcpStream>, 
+}
+
+fn tcp_read_simple(value: &mut TcpStream, cache: &mut [u8]) {
+    match value.read(cache.as_mut()) {
+        Ok(s) => {
+            println!("[-] info: read replies from server. "); 
+            let c = String::from_utf8_lossy(&cache[..s]); 
+            println!("{}", c); 
+        },
+        Err(e) => {
+            println!("[-] info: read failure - {:?}", e)
+        },
+    } 
+}
