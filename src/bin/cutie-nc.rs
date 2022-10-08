6b3c3ffd6438a845d62fd9080e4819911036b28c
Cutie Deng <CutieDeng@iCloud.com>
2022-10-08 22:21:06 +0800

    新增了对 udp 链接的支持！
    - 支持通过 uconnect 关键字进行 udp 连接.

--- src/bin/cutie-nc.rs
+++ src/bin/cutie-nc.rs
@@ -1,7 +1,7 @@
-use std::{net::{SocketAddrV4, Ipv4Addr, TcpStream}, io::{stdin, Write, Read}, fs::File, time::Duration};
+use std::{net::{SocketAddrV4, Ipv4Addr, TcpStream, UdpSocket}, io::{stdin, Write, Read}, fs::File, time::Duration};
 
 fn main() {
-    let mut connect_config = Box::new(ConnectConfig { target: None, }); 
+    let mut connect_config = Box::new(ConnectConfig::Nothing); 
     let mut input = String::new(); 
     let mut cache = Box::new([0u8; 1024]); 
     loop {
@@ -50,7 +50,8 @@ fn main() {
                         match new_stream {
                             Ok(n) => {
                                 n.set_read_timeout(Some(Duration::from_secs(1))).unwrap(); 
-                                connect_config.target = Some(n); 
+                                // connect_config.target = Some(n); 
+                                *connect_config = ConnectConfig::Tcp(n); 
                                 println!("[-] connect build. "); 
                             },
                             Err(e) => {
@@ -60,10 +61,24 @@ fn main() {
                     }
                 }
             }
-        } else if input.starts_with("file ") {
-            match connect_config.as_mut().target {
-                Some(ref mut tcp_stream) => {
-                    let input = (&input[5..]).trim_end(); 
+        // } else if input.starts_with("uconnect ") { 
+        } else if let Some(input) = input.strip_prefix("uconnect ") { 
+            let others = input.trim_end();
+            let u = UdpSocket::bind("0.0.0.0:0").unwrap();
+            let u2 = u.connect(others);
+            match u2 {
+                Ok(()) => {
+                    u.set_read_timeout(Some(Duration::from_secs(1))).unwrap(); 
+                    *connect_config = ConnectConfig::Udp(u); 
+                },
+                Err(e) => {
+                    println!("\x1b[31;1m[e] error: {:?}\x1b[0m", e); 
+                },
+            }
+        } else if let Some(input) = input.strip_prefix("file ") { 
+            match connect_config.as_mut() {
+                ConnectConfig::Tcp(ref mut tcp_stream) => { 
+                    let input = input.trim_end(); 
                     let f = File::open(input);
                     match f {
                         Ok(mut actual_file) => {
@@ -88,12 +103,40 @@ fn main() {
                         },
                     }
                 }
-                None => println!("\x1b[31;1m[e] error: invalid status for sending file, because connection doesn't exist. \x1b[0m"), 
+                ConnectConfig::Udp(ref mut udp_socket) => {
+                    let input = input.trim_end();
+                    let f = File::open(input);
+                    match f {
+                        Ok(mut actual_file) => {
+                            loop {
+                                let r = actual_file.read(cache.as_mut_slice()).unwrap(); 
+                                if r == 0 { 
+                                    println!("[-] finish the file block write. "); 
+                                    break 
+                                }
+                                match udp_socket.send(&cache[0..r]) {
+                                    Ok(_) => {},
+                                    Err(e) => {
+                                        println!("\x1b[31;1m[e] error: write {:?}\x1b[0m", e); 
+                                        break 
+                                    },
+                                }
+                            }
+                            udp_read_simple(udp_socket, cache.as_mut_slice()); 
+                        },
+                        Err(e) => {
+                            println!("\x1b[31;1m[e] error: file open failure - {:?}\x1b[0m", e); 
+                        },
+                    }
+                }
+                ConnectConfig::Nothing => println!("\x1b[31;1m[e] error: invalid status for sending file, because connection doesn't exist. \x1b[0m"), 
             }
-        } else if input.starts_with(" ") {
-            match connect_config.as_mut().target {
-                Some(ref mut tcp_stream) => {
-                    match tcp_stream.write((&input[1..]).as_bytes()) {
+        } else if input.starts_with(' ') {
+            // match connect_config.as_mut().target {
+            match connect_config.as_mut() { 
+                ConnectConfig::Tcp(ref mut tcp_stream) => { 
+                // Some(ref mut tcp_stream) => {
+                    match tcp_stream.write(input[1..].as_bytes()) {
                         Ok(_) => {
                         },
                         Err(e) => {
@@ -102,7 +145,17 @@ fn main() {
                     }
                     tcp_read_simple(tcp_stream, cache.as_mut_slice()); 
                 }
-                None => println!("\x1b[31;1m[e] error: invalid status for sending info, because connection doesn't exist. \x1b[0m"), 
+                ConnectConfig::Nothing => println!("\x1b[31;1m[e] error: invalid status for sending info, because connection doesn't exist. \x1b[0m"), 
+                ConnectConfig::Udp(ref mut udp_socket) => {
+                    // udp_socket.send_to(buf, addr)
+                    match udp_socket.send(input[1..].as_bytes()) {
+                        Ok(_) => {},
+                        Err(e) => {
+                            println!("\x1b[31;1m[e] error: {:?}\x1b[0m", e)
+                        },
+                    }
+                    udp_read_simple(udp_socket, cache.as_mut_slice()); 
+                }
             }
         } else if input.starts_with("help") {
             println!("cutie-nc, version {}\n", env!("CARGO_PKG_VERSION")); 
@@ -117,9 +170,15 @@ fn main() {
     }
 }
 
-struct ConnectConfig {
-    target: Option<TcpStream>, 
-}
+// struct ConnectConfig {
+//     target: Option<TcpStream>, 
+// }
+
+enum ConnectConfig {
+    Nothing, 
+    Tcp(TcpStream), 
+    Udp(UdpSocket), 
+} 
 
 fn tcp_read_simple(value: &mut TcpStream, cache: &mut [u8]) {
     let mut continue_flag = true; 
@@ -138,5 +197,23 @@ fn tcp_read_simple(value: &mut TcpStream, cache: &mut [u8]) {
                 println!("[-] info: read failure - {:?}", e)
             },
         } 
+    }
+}
+
+fn udp_read_simple(value: &mut UdpSocket, cache: &mut [u8]) {
+    let mut continue_flag = true; 
+    while continue_flag {
+        match value.recv(cache) {
+            Ok(s) => {
+                if s < cache.len() { continue_flag = false } 
+                println!("[-] info: read replies from server. "); 
+                let c = String::from_utf8_lossy(&cache[..s]); 
+                println!("{}", c); 
+            }
+            Err(e) => {
+                continue_flag = false; 
+                println!("[-] info: read failure - {:?}", e); 
+            }
+        }
     }
 }
